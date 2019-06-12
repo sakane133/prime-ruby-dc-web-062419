e1405948220d6752e0c637e0dfbf11e262a254cd
Samantha Kane <s.kane133@gmail.com>
2019-06-12 03:17:32 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(number)
    if number < 2
      return false
-    else if (0...number).each do |num|
+    else if (2...number).each do |num|
       if number % num == 0 
       return false
    end
