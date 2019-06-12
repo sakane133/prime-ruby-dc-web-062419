d209a15a374e6e438a93f77812ab636ab46983eb
Samantha Kane <s.kane133@gmail.com>
2019-06-12 03:19:45 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+   if number < 2
+     return false
+    elsif (2...number).each do |num|
+      if number % num == 0 
+      return false
+    end
+   end
+  end
+true
+end
